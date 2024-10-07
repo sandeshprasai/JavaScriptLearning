c7b1d046912c60a57f20c1bead854e88753ebb4b
sandeshprasai <sandeshprasai4321@gmail.com>
2024-10-07 15:32:46 +0545

    Classes and Objects

---
+++ ClassesAndObjects/app.js
@@ -0,0 +1,10 @@
+const student = 
+{
+    name : "Sandesh Prasai",
+    marks: 94.4,
+    getMarks :  function() {console.log( this.marks)},
+}
+
+console.log(student)
+console.log(student.marks)
+console.log(student.getMarks())
